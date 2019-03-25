144d203e0d390fe4c855d38da56e7b986b63b04e
gabybdc <36209205+gabybdc@users.noreply.github.com>
2019-03-25 14:53:28 -0600

    Test file to print variables in python

---
+++ test.py
@@ -0,0 +1,7 @@
+b = 2     # b entero
+print(b)
+b = 2.0   # b flotante 
+print(b)
+b = '2.0'   # b es un caracter
+print(b)
+print(b[0:2]) # Selecciona los primeros dos caracteres de la variable s1 con posición 0 y 1
